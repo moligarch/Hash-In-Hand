e515d542ded11c0a4ffe1646408b9e951a12a998
Moligarch <mr.verkiani@gmail.com>
2024-11-07 19:39:48 +0330

    Revise versioning

--- resource.h
+++ resource.h
@@ -2,8 +2,8 @@
 // Microsoft Visual C++ generated include file.
 // Used by HashInHandLib.rc
 
-#define VER_PRODUCTVERSION          0,1,0,0
-#define VER_PRODUCTVERSION_STR      "0.1.0.0\0"
+#define VER_PRODUCTVERSION          0,1,0
+#define VER_PRODUCTVERSION_STR      "0.1.0\0"
 
 #define COMPANY_STR                 "Moein Verkiani (@Moligarch)"
 #define COPYRIGHT_STR               "Copyright (C) 2024 Moein Verkiani (@Moligarch)"
